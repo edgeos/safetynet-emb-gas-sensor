9bf06f82246bbc14a7e133cdc1c9b071557f4f17
Steve Wik <steve.wik@hiddenlevel.xyz>
2019-01-17 11:40:57 -0500

    add: parameter for a4 in gas sensor transfer function

--- ble-dev/src/aducm355/aducm355_gas_constants.c
+++ ble-dev/src/aducm355/aducm355_gas_constants.c
@@ -9,20 +9,20 @@
 SensorSettings MeasureSettings[NUM_GAS_SENSORS*NUM_FREQS_PER_SENSOR] = 
 {
    // {SENSOR#, FREQ [Hz], NUM_TO_AVG, A0, A1, A2, A3, A4, A5, HEATER_ALWAYS_ON, MUX_ENABLE, MUX_S0, MUX_S1}
-   {1, 10000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false}, 
-   {1, 15000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false}, 
-   {1, 20000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {1, 25000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {2, 30000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {2, 35000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {2, 40000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {2, 45000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {3, 50000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {3, 55000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {3, 60000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {3, 65000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {4, 70000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {4, 75000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {4, 80000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false},
-   {4, 85000.0, 7, 1.0, -2.0, 3.0, -4.0, false, false, false, false}
+   {1, 10000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false}, 
+   {1, 15000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false}, 
+   {1, 20000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {1, 25000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {2, 30000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {2, 35000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {2, 40000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {2, 45000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {3, 50000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {3, 55000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {3, 60000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {3, 65000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {4, 70000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {4, 75000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {4, 80000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false},
+   {4, 85000.0, 7, 1.0, -2.0, 3.0, -4.0, 5.0, false, false, false, false}
 };
